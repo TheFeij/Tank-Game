10f776e0d7d396d4ec0528dfa2f186c1ec8a431a
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-27 22:47:24 +0330

    added game configuration constants

---
+++ config/config.go
@@ -0,0 +1,18 @@
+package config
+
+// game configurations
+const (
+	TankWidth    = 70
+	TankHeight   = 70
+	TankSpeed    = 4
+	TankAmmo     = 30
+	BulletWidth  = 5
+	BulletHeight = 10
+	BulletSpeed  = 5
+	BombWidth    = 30
+	BombHeight   = 30
+	BombSpeed    = 2
+	ScreenWidth  = 640
+	ScreenHeight = 480
+	GameTitle    = "Tank Game"
+)
