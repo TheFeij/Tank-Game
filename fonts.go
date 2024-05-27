fbb5fb318a0417d8614e60285941784a319b006f
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-27 22:55:56 +0330

    defined initFonts and loadFont functions

---
+++ fonts.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"golang.org/x/image/font"
+	"golang.org/x/image/font/opentype"
+	"io/ioutil"
+	"log"
+)
+
+// resultFont for the end of the game message indicating win or loss
+var resultFont font.Face
+
+// scoreFont for displaying the score while game is paused or finished
+var scoreFont font.Face
+
+// statusFont for displaying status information on the top left of the screen during the game
+var statusFont font.Face
+
+// initFonts initializes different fonts of the game
+func initFonts() {
+	var err error
+
+	resultFont, err = loadFont("./assets/MightySouly-lxggD.ttf", 36)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scoreFont, err = loadFont("./assets/MightySouly-lxggD.ttf", 48)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	statusFont, err = loadFont("./assets/MightySouly-lxggD.ttf", 20)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// loadFont loads a font from a given file path and returns it as a font.Face with the specified size.
+func loadFont(filePath string, size float64) (font.Face, error) {
+	fontBytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+	f, err := opentype.Parse(fontBytes)
+	if err != nil {
+		return nil, err
+	}
+	const dpi = 72
+	return opentype.NewFace(f, &opentype.FaceOptions{
+		Size:    size,
+		DPI:     dpi,
+		Hinting: font.HintingFull,
+	})
+}
--- go.mod
+++ go.mod
@@ -1,1 +1,20 @@
 module Tank-Game
+
+go 1.22.2
+
+require (
+	github.com/ebitengine/oto/v3 v3.2.0
+	github.com/hajimehoshi/ebiten/v2 v2.7.4
+	github.com/hajimehoshi/go-mp3 v0.3.4
+	golang.org/x/image v0.16.0
+)
+
+require (
+	github.com/ebitengine/gomobile v0.0.0-20240518074828-e86332849895 // indirect
+	github.com/ebitengine/hideconsole v1.0.0 // indirect
+	github.com/ebitengine/purego v0.7.0 // indirect
+	github.com/jezek/xgb v1.1.1 // indirect
+	golang.org/x/sync v0.7.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+)
