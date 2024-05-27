d53a666fd5cbc37e9c4715fc033020505f901ef2
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-27 22:47:42 +0330

    defined a Direction Type with its possible values

---
+++ components/directions.go
@@ -0,0 +1,11 @@
+package components
+
+// Direction represents the possible movement directions in the game.
+type Direction string
+
+const (
+	Up    Direction = "up"
+	Down            = "down"
+	Right           = "right"
+	Left            = "left"
+)
