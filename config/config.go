88d72bbd181fb38ec35a4cf2f2c4597d587326fb
thefeij <abolfazl.moradi.feijani@gmail.com>
2024-05-27 23:28:55 +0330

    added TankImagePath and BombImagePath constants

--- config/config.go
+++ config/config.go
@@ -2,17 +2,19 @@ package config
 
 // game configurations
 const (
-	TankWidth    = 70
-	TankHeight   = 70
-	TankSpeed    = 4
-	TankAmmo     = 30
-	BulletWidth  = 5
-	BulletHeight = 10
-	BulletSpeed  = 5
-	BombWidth    = 30
-	BombHeight   = 30
-	BombSpeed    = 2
-	ScreenWidth  = 640
-	ScreenHeight = 480
-	GameTitle    = "Tank Game"
+	TankWidth     = 70
+	TankHeight    = 70
+	TankSpeed     = 4
+	TankAmmo      = 30
+	BulletWidth   = 5
+	BulletHeight  = 10
+	BulletSpeed   = 5
+	BombWidth     = 30
+	BombHeight    = 30
+	BombSpeed     = 2
+	ScreenWidth   = 640
+	ScreenHeight  = 480
+	GameTitle     = "Tank Game"
+	TankImagePath = "./assets/tank.png"
+	BombImagePath = "./assets/bomb.png"
 )
